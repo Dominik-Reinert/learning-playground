f12e53af497172a533f8a691d78adbab78228550
Dominik Reinert <dominik.reinert.merzig@googlemail.com>
2020-06-14 10:53:30 +0200

    add npm install to the install tools script

--- tools/install_tools.sh
+++ tools/install_tools.sh
@@ -12,4 +12,9 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update && sudo apt install yarn
 
+echo "Install npm"
+sudo apt-install npm
+
+
+
 echo "~~~ done! ~~~"
