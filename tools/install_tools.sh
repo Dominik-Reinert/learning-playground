6d8c640602b700307dff2b477fcf9976e574935d
Dominik Reinert <dominik.reinert.merzig@googlemail.com>
2020-06-14 10:42:35 +0200

    add a tools directory with a script that auto installs yarn packages and makes sure, WSL works

---
+++ tools/install_tools.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Run this script to install all the deps needed";
+
+echo "Removing gpg and add gnupg1 to be able to work on WSL"
+sudo apt remove gpg
+echo "add gnupg1 to be able to work on WSL"
+sudo apt install gnupg1
+
+echo "Install yarn";
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update && sudo apt install yarn
+
+echo "~~~ done! ~~~"
