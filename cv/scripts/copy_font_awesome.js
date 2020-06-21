95a81758ebfe45cdb43aa7f8b900cf4602f2f8a2
Dominik Reinert <dominik.reinert.merzig@googlemail.com>
2020-06-21 13:14:10 +0200

    make the copy command work again and make it a litle more error prone

--- cv/scripts/copy_font_awesome.js
+++ cv/scripts/copy_font_awesome.js
@@ -5,11 +5,29 @@ const path = require("path");
 
 (function copyFontAwesome() {
   const sourcePath = path.resolve(
-    "../node_modules/@fortawesome/fontawesome-free/css/all.min.css"
+    "./node_modules/@fortawesome/fontawesome-free/css/all.min.css"
   );
   console.log(`source: ${sourcePath}`);
+  fs.access(sourcePath, fs.F_OK, (err) => {
+    if (err) {
+      console.log(`source file does not exist!`);
+      console.error(err);
+      return;
+    }
 
-  const targetPath = path.resolve(`../public/font-awesome.css`);
-  console.log(`Copy fontawesome css to ${targetPath}`);
-  fs.createReadStream(sourcePath).pipe(fs.createWriteStream(targetPath));
+    const targetPath = path.resolve(`./public/font-awesome.css`);
+    console.log(`Copy fontawesome css to ${targetPath}`);
+
+    fs.access(targetPath, fs.F_OK, (err) => {
+      if (err) {
+        console.log(`target file/path does not exist. Creating it`);
+
+        if (!fs.existsSync(dir)) {
+          fs.mkdirSync(dir);
+        }
+      }
+
+      fs.createReadStream(sourcePath).pipe(fs.createWriteStream(targetPath));
+    });
+  });
 })();
