7cd97881f5b6995c8734312c39c681136bc0ffff
Dominik Reinert <dominik.reinert.merzig@googlemail.com>
2020-06-28 10:23:46 +0200

    add the proper rgba value to the modal

--- cv/src/modal/modal.tsx
+++ cv/src/modal/modal.tsx
@@ -23,20 +23,41 @@ export const Modal = (props: React.PropsWithChildren<ModalProps>) => {
 
 const useModalStyle = (open: boolean) => {
   const theme = usePageBaseTheme();
+
+  const hexToRgb = (hex) => {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result
+      ? {
+          r: parseInt(result[1], 16),
+          g: parseInt(result[2], 16),
+          b: parseInt(result[3], 16),
+        }
+      : undefined;
+  };
+
+  const backgroundRgb = hexToRgb(theme.mainColors.darkest);
+  const rgbaValue = backgroundRgb
+    ? `rgba(${backgroundRgb.r}, ${backgroundRgb.g}, ${backgroundRgb.b}, 0.8)`
+    : "";
+
   return css`
     label: modal;
 
-    display: ${open ? "block" : "hidden"};
+    display: ${open ? "flex" : "none"};
 
     position: absolute;
-    opacity: 0.4;
-    background-color: ${theme.mainColors.darkest};
+    z-index: 1;
+    background-color: ${rgbaValue};
 
-    min-width: 200px;
-    min-height: 300px;
+    width: 100%;
+    height: 100%;
 
     .modal-content {
       background-color: ${theme.background};
+
+      width: 200px;
+      height: 300px;
+      margin: auto;
     }
   `;
 };
