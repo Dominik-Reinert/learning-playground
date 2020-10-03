a1f1986e33ba0614ebcf110e0a501a871a664d46
Dominik Reinert <dominik.reinert.merzig@googlemail.com>
2020-10-03 22:47:57 +0200

    add some styling to the homepage buttons

--- cv/src/pages/home_page.tsx
+++ cv/src/pages/home_page.tsx
@@ -1,17 +1,49 @@
+import { css } from "@emotion/core";
 import { Link } from "react-router-dom";
+import { usePageBaseTheme } from "../hooks/use_page_base_theme";
 import { RouteURL } from "../router/router";
 
 export const HomePageComponent = () => {
+  const homepageStyle = useHomepageStyle();
   return (
-    <div>
-      <div>
-        <div>
-          <Link to={`${RouteURL.CV}`}>CV</Link>
+    <div css={homepageStyle}>
+      <div className="links">
+        <div className="link-wrapper">
+          <Link to={`${RouteURL.CV}`}>Curriculum vitae</Link>
         </div>
-        <div>
+        <div className="link-wrapper">
           <Link to={`${RouteURL.NEWSLETTER}`}>Newsletter</Link>
         </div>
       </div>
     </div>
   );
+};
+
+const useHomepageStyle = () => {
+  const theme = usePageBaseTheme();
+  return css`
+    label: homepage;
+
+    display: flex;
+    flex-direction: column;
+
+    .links {
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+    }
+
+    .link-wrapper {
+      cursor: pointer;
+      color: ${theme.mainColors.darker};
+      background-color: ${theme.mainColors.ligthest};
+      padding: 6px;
+      margin: 18px;
+
+      a {
+        text-decoration: none;
+        color: ${theme.mainColors.darker};
+      }
+    }
+  `;
 };
