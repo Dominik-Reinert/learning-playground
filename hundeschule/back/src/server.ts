905c1923a372e3aba18593bc0e2d3363df6a251a
Dominik Reinert <dominik.reinert.merzig@googlemail.com>
2020-11-22 17:34:40 +0100

    add cors policy | remove stuff from create-react app

--- hundeschule/back/src/server.ts
+++ hundeschule/back/src/server.ts
@@ -3,8 +3,10 @@
 import express from "express";
 import "express-async-errors";
 import path from "path";
+import cors from "cors";
 
 const app = express();
+app.use(cors({ origin: "http://localhost:8080" }));
 
 app.use(express.static(path.join(__dirname, "../dist")));
 
