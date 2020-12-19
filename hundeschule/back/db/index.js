7a1046d7a1f5f7ed5e0ef96b27406fb4c458a242
Dominik Reinert <dominik.reinert.merzig@googlemail.com>
2020-12-19 06:38:19 +0100

    move the db stuff to the backend folder

--- hundeschule/back/db/index.js
+++ hundeschule/back/db/index.js
@@ -2,7 +2,7 @@
 
 const { initialSetup } = require('./dist/initial_setup');
 const { createTables } = require('./dist/create_tables');
-const creds = require('../creds/postgresql.creds.json');
+const creds = require('../../creds/postgresql.creds.json');
 
 (async function start() {
     const { user, password } = creds;
