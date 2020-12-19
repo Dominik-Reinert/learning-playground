5a91bcfe07d1cdc729854cfcdf7f233b5a1c32fd
Dominik Reinert <dominik.reinert.merzig@googlemail.com>
2020-12-19 06:48:44 +0100

    move be index file out of dist

--- hundeschule/back/src/server.ts
+++ hundeschule/back/src/server.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
+import cors from "cors";
 import express from "express";
 import "express-async-errors";
 import path from "path";
-import cors from "cors";
 
 const app = express();
 app.use(cors({ origin: "http://localhost:8080" }));
@@ -15,7 +15,7 @@ app.get("/ping", function (req, res) {
 });
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "../dist", "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(process.env.PORT || 3000);
